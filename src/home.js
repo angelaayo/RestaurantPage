02671ec048122bc919483d1c6c6bd85ae5ce0e53
Angela <angelaayodeji@gmail.com>
2026-03-25 12:28:46 -0500

    linked home page elements to DOM

--- src/home.js
+++ src/home.js
@@ -1,14 +1,26 @@
 import mainPastaImg from "./mainPasta.jpg";
 export function createHomePage(){
     const homeContainer = document.createElement("div");
+    homeContainer.classList.add("homeContainer");
     const imageHolder = document.createElement("img");
+    imageHolder.classList.add("mainImg");
     imageHolder.src = mainPastaImg;
-    const infoDump = document.createElement("div");
+    const infoDump = document.createElement("div"); //container for goal and history
+    infoDump.classList.add("infoContainer");
     const ourGoal = document.createElement("div");
-    ourGoal.textContent = "";
-    const ourMission = document.createElement("div");
+    const goalHeader = document.createElement("div");
+    goalHeader.textContent = "Our Goal"
+    ourGoal.textContent = "To serve up hearty, homemade-style spaghetti that keeps you coming back for more — because good pasta fixes everything.";
+    const ourHistory = document.createElement("div");
+    const historyHeader = document.createElement("div");
+    historyHeader.textContent = "Our History"
+    ourHistory.textContent = "Spag Spot was born out of a simple love for spaghetti and good company, started by two friends who believed great pasta shouldn't be complicated."
+    infoDump.append(goalHeader, ourGoal, historyHeader, ourHistory);
+    const faveHeader = document.createElement("div");
+    faveHeader.textContent = "Our Favorites";
     //grid container
     const faveContainer = document.createElement("div");
+    faveContainer.classList.add("faveContainer");
     //const foodImages = ["salad.png", "burger.png", "tiramisu.png"];
     for(let i=0; i<3; i++){
         const foodContainer = document.createElement('div');
@@ -16,8 +28,10 @@ export function createHomePage(){
         //foodImg.src = foodImages[i];
         const foodTitle = document.createElement("div");
         const foodDescription = document.createElement("div");
+        foodContainer.append(foodImg, foodTitle, foodDescription);
+        faveContainer.append(foodContainer);
     }
-    homeContainer.append(imageHolder);
+    homeContainer.append(imageHolder, infoDump, faveHeader, faveContainer);
     return homeContainer;
     
 }
