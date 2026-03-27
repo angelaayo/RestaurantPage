ecf3ade64d5a44cb192527ca363cb425bfed7f9d
Angela <angelaayodeji@gmail.com>
2026-03-26 22:47:43 -0500

    started menu page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import "./style.css";
 import {createHomePage} from "./home.js";
+import {createMenuPage} from "./menu.js";
+import {createContactPage} from "./contact.js";
 
 const navigate = (()=>{
-    const outerContainer = document.querySelector("#content");
-    outerContainer.append(createHomePage());
+    const contentContainer = document.querySelector("#content");
+    const homeBtn = document.querySelector("#homeBtn");
+    const menuBtn = document.querySelector("#menuBtn");
+    const contactBtn = document.querySelector("#contactBtn");
+    contentContainer.append(createHomePage());
+
+    homeBtn.addEventListener("click", ()=>{
+        contentContainer.innerHTML = "";
+        contentContainer.append(createHomePage());
+    });
+
+    menuBtn.addEventListener("click", ()=>{
+        contentContainer.innerHTML = "";
+        //contentContainer.append(createMenuPage());
+    });
+
+    contactBtn.addEventListener("click", ()=>{
+        contentContainer.innerHTML = "";
+        //contentContainer.append(createContactPage());
+    });
 })();
