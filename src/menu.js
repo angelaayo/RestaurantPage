04e6fbc72459eff3c9fcb3c102895e522535fd7f
Angela <angelaayodeji@gmail.com>
2026-03-26 23:32:45 -0500

    added menuData file

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,10 @@
-export function createMenuPage (){
-    
+import {menuData} from "./menuData.js";
+export function createMenuPage (menuData){
+    const menuHeader = document.createElement("div");
+    const mainContainer = document.createElement("div");
+
+    menuData.forEach(item => {
+        const textContainer = createTextBlock(item);
+        const imageHolder = createImageBlock(item.src)
+    });
 }
---
+++ src/menuData.js
@@ -0,0 +1,11 @@
+export const menuData = [
+    {
+        title: "Wine",
+        //src:,
+        items: [
+            //{name: "", price: },
+            //{name: "", price: },
+            //{name: "", price: },
+        ]
+    }
+]
